f667e472c4e1680c473f08ca01f98f2280666d29
cannibalcheeseburger <kash.pegasus@gmail.com>
2023-08-24 23:00:06 +0530

    1692898206

--- getConfig.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# conf=("/etc/fstab" "/etc/samba/smb.conf")
-readarray -t conf <  ./conf.txt
-for con in  "${conf[@]}"
-do
-  cp -f $con ./
-done
---
+++ prototype/modified.sh
@@ -0,0 +1,2 @@
+#find /hdd/photoarchive/ -type f -name "IMG*" > log.txt
+exiftool "-filemodifydate<datetimeoriginal"
